5f665e7b07bd1790a33e0544da5e2739a61e40eb
Keith Suderman <suderman@anc.org>
2014-12-10 15:44:39 -0500

    Aligned version number with non-experimental version. Added new WebService interface.

--- src/main/java/org/lappsgrid/experimental/api/WebService.java
+++ src/main/java/org/lappsgrid/experimental/api/WebService.java
@@ -1,6 +1,15 @@
 package org.lappsgrid.experimental.api;
 
 /**
+ * Interface for all services deployed to the LAPPS grid.
+ * <p>
+ * A service accepts a JSON object as input and produces a JSON object
+ * as output.  The JSON objects must use the LAPPS Interchange Format (LIF)
+ * with the <i>discriminator</i> set to one of the LIF query URI.
+ * <p>
+ * See http://vocab.lappsgrid.org/discriminators.html for a list of all
+ * URI used by LAPPS.
+ *
  * @author Keith Suderman
  */
 public interface WebService
