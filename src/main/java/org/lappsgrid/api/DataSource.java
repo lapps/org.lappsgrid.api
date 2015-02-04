0e007ac3a83a75b77a7fe924a941862dc99ab5ff
Keith Suderman <suderman@anc.org>
2015-02-04 18:19:03 -0500

    Added DataSource and ProcessingService interfaces.

---
+++ src/main/java/org/lappsgrid/api/DataSource.java
@@ -0,0 +1,11 @@
+package org.lappsgrid.api;
+
+/**
+ * A naming interface for LAPPS services that act as sources of data
+ * for other LAPPS services.
+ *
+ * @author Keith Suderman
+ */
+public interface DataSource extends WebService
+{
+}
---
+++ src/main/java/org/lappsgrid/api/ProcessingService.java
@@ -0,0 +1,11 @@
+package org.lappsgrid.api;
+
+/**
+ * A naming interface for LAPPS services that transform their input data
+ * in some way.
+ *
+ * @author Keith Suderman
+ */
+public interface ProcessingService extends WebService
+{
+}
